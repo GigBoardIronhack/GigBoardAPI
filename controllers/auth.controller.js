4075f884bfe7d51d5f24887b7cbabfe178e2186a
Juan Manuel <j-grafi@hotmail.com>
2025-02-10 12:47:49 +0100

    feat 10% user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,7 +4,6 @@ const bcrypt = require("bcrypt");
 module.exports.register = async (req, res, next) => {
   try {
     const user = await User.create(req.body);
-
     res.status(201).json(user);
   } catch (error) {
     next(error);
